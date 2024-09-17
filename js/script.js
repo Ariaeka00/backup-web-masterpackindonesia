1c9007fc662b0fbbe74f66f43b982b76f72c45c6
ariaeka <ariaeka089@gmail.com>
2024-09-17 12:42:14 +0700

    menambahkan fitur search dan product section

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@ const navbarNav = document.querySelector('.navbar-nav')
 
 const hamburgerMenu = document.querySelector('#hamburger-menu')
 
-hamburgerMenu.addEventListener('click', () => {
+hamburgerMenu.addEventListener('click', (e) => {
     navbarNav.classList.toggle('active')
+    e.preventDefault()
 })
 
 // klik diluat side bar untuk menghilangkan nav
@@ -12,3 +13,22 @@ document.addEventListener('click', (e) => {
         navbarNav.classList.remove('active')
     }
 })
+
+// Search Form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.getElementById('search-box')
+
+document.querySelector('#search').onclick = (e) => {
+    searchForm.classList.toggle('active')
+    searchBox.focus()
+    e.preventDefault()
+}
+
+// click untuk menghilangkan form search di luar button search
+document.addEventListener('click', (e) => {
+    if(!search.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active')
+    }
+})
+
+
